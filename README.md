4a67c997c420ca1ffe44f2ee942958a6fef861a1
Bharathi Athinarayanan <a.bharathi@gmail.com>
2017-06-13 13:04:02 +0530

    V5
    Refined feedreader.js and Readme.md

--- README.md
+++ README.md
@@ -3,24 +3,23 @@
 This project uses jasmine to test the functionality of a feed reader website.
 
 ### Instructions
-To view the feedreader website download all the files and open index.html in your browser.
+To view the feedreader testing site on github pages go to https://rathisoft.github.io/Bharathi_FEND_feedreader_Testing/
+                        or
+Download the repo locally and run the index.html
 
-Alternatively, you can view the site on github pages by using the link below.
 
 ## Tests
 RSS Feeds:
-* Feeds are Defined
-* Feed URL is not empty
-* Feed names are not empty
+* Feeds are Defined (This is the first test) --> a test to make sure that the "allFeeds" variable has been defined and that it is not empty.
+* URL Validation (This is the second test) --> a test that loops through each feed in the "allFeeds" object and ensures it has a URL defined and that the URL is not empty.
+* Name validation (This is the third test) --> a test that loops through each feed in the allFeeds object and ensures it has a name defined and that the name is not empty.
 
 The Menu:
-* The menu is hidden by default
-* When clicked the menu changes visibility
+* Menu validation Part 1 (This is the fourth test) --> a test that ensures that the menu element is hidden by default.
+* Menu validation Part 2 (This is the fifth test) --> a test that ensure changes in visibility when the hamburger menu icon is clicked.
 
-Initial Entries:
-* Feed container has at least ONE entry
 
-New Feed Selection:
-* Feed content changes when new feed is selected
+Initial entries (This is the sixth test) --> a test that ensures when the loadFeed function is called and completes its work, there is at least a single ".entry" element within the .feed"container.
 
-To view the feedreader testing site on github pages go to https://rathisoft.github.io/Bharathi_FEND_feedreader_Testing/.
+New Feed Selection valiation (This is the seventh test) --> a test that ensures when a new feed is loaded by the "loadFeed" function the content actually changes.
+
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -8,12 +8,12 @@
  * to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
+    /* This is the first test suite - a test suite just contains
      * a related set of tests. This suite is all about the RSS
-     * feeds definitions, the allFeeds variable in our application.
+     * feeds definitions, the allFeeds variable in the application.
      */
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
+        /* This is the first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty.
          */
@@ -22,22 +22,28 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-        // URL Validation
-        it('- should not have blank / empty URL -> URL Validation', function() {
+        /* URL Validation (This is the second test) --> a test that loops through
+         * each feed in the allFeeds object and ensures it has a URL defined and
+         * that the URL is not empty.
+         */
+        it('URL Validation -> ensure the feeds to not have blank / empty URL', function() {
             allFeeds.forEach(function(feed) {
-                expect(feed.url).toBeDefined();
-                expect(feed.url.length).not.toBe(0);
+                expect(feed.url).toBeDefined(); // check for a properly defined URL
+                expect(feed.url.length).not.toBe(0); // check for empty URL
                 expect(feed.url).not.toBe(''); // Additional check for empty string
             });
         });
 
-        // Name validation
-        it('- should not have blank / empty names -> NAME validation', function() {
+        /* Name validation (This is the third test) --> a test that loops through
+         * each feed in the allFeeds object and ensures it has a name defined and
+         * that the name is not empty.
+         */
+        it('Name validation -> ensure the feeds to not have blank / empty names', function() {
             allFeeds.forEach(function(feed) {
-                expect(feed.name).toBeDefined();
-                expect(typeof feed.name).toEqual('string');
-                expect(feed.name.length).toBeGreaterThan(0);
-                expect(feed.name).not.toBe('');
+                expect(feed.name).toBeDefined(); // check for a properly defined name
+                expect(typeof feed.name).toEqual('string'); // checks whether the name element is a string or not
+                expect(feed.name.length).toBeGreaterThan(0); // checks whether the name string is empty or not by mandating its length to be greater than zero
+                expect(feed.name).not.toBe(''); // Additional check for empty string
             });
         });
     });
@@ -46,49 +52,50 @@ $(function() {
     // Menu test suite.
     describe('The menu', function() {
 
-        // This test ensures the menu element is hidden by default.
-        it('- ensure the menu element is hidden by default -> Menu validation', function() {
+        /* Menu validation Part 1 (This is the fourth test) --> a test that ensures that
+         * the menu element is hidden by default.
+         */
+        it('Menu validation Part 1 -> ensure the menu element is hidden by default', function() {
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
 
-        // This test validates proper functioning of the  menu toggle.
-        it('- changes visibility when clicked -> Menu validation', function() {
-            var menu_Icon = $('.menu-icon-link');
+        /* Menu validation Part 2 (This is the fifth test) --> a test that ensure changes in
+         * visibility when the hamburger menu icon is clicked.
+         */
+        it('Menu validation Part 2 -> ensure menu changes visibility when the menu icon is clicked', function() {
+            var menu = $('.menu-icon-link');
 
-            // This tests for menu display.
-            menu_Icon.click();
+.
+            menu.click();             // Test to check whether menu gets displayed or not when the hamburger menu icon is clicked
             expect($('body').hasClass('menu-hidden')).toBe(false);
 
-            // This tests for menu hide.
-            menu_Icon.click();
+
+            menu.click();             // Test to check whether menu gets hidden or not when the hamburger menu icon is clicked again
             expect($('body').hasClass('menu-hidden')).toBe(true);
         });
     });
 
-    // Initial entries test suite.
-    describe('Initial Entries', function() {
-
-        /* Test that ensures when the loadFeed
+        /* Initial entries (This is the sixth test) a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+    describe('Initial Entries', function() {
+
         beforeEach(function(done) {
             loadFeed(0, function() {
                 done();
             });
         });
 
-        // tests that there is at least one entry in feed.
-        it('should contain at least one feed entry -> Feed Entry validation.', function() {
+        it('Feed Entry validation -> esnure there is at least a single entry element within the feed container', function() {
             expect($('.entry').length).toBeGreaterThan(0);
         });
     });
-
+        /* New Feed Selection valiation (This is the seventh test) -> a test that ensures when a new feed is loaded
+         * by the loadFeed function that the content actually changes.
+         */
     describe('New Feed Selection', function() {
 
-        // tests that new content is loaded by loadFeed().
         var $feedFirst;
         var $feedSecond;
 
@@ -99,7 +106,7 @@ $(function() {
             });
         });
 
-        it('should change feed content -> feed content change validation', function(done) {
+        it('Feed content change validation --> ensure when a new feed is loaded by the loadFeed function that the content actually changes', function(done) {
             loadFeed(1, function() {
                 feedSecond = $('.feed').html();
                 expect(feedSecond).not.toEqual(feedFirst);
